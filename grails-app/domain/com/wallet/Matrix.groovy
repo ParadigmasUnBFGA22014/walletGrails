784c887d0cbf556c31ece41c6aafdfb9f395db18
Álex Mesquita <alex.mesquita06@hotmail.com>
2014-09-03 12:30:35 -0300

    Classes implementadas por ramon

---
+++ grails-app/domain/com/wallet/Matrix.groovy
@@ -0,0 +1,7 @@
+package com.wallet
+
+class Matrix {
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/com/wallet/Statistical.groovy
@@ -0,0 +1,64 @@
+package com.wallet
+
+class Statistical {
+
+
+ static double calculeAvarange(List<Double> values)
+
+	{
+
+		double avarange=0.f;
+
+		for(int i=0;i<values.size();i++)
+
+		{
+
+			avarange+=values[i]/values.size();
+
+		}
+		return avarange;
+
+	}
+
+	
+
+ static double calculeVariance(List<Double> values)
+
+	{
+
+		double variance=0.f;
+
+		double avarange=Statistical.calculeAvarange(values);
+
+		
+
+		for(int i=0;i<values.size();i++)
+
+		{
+
+			variance+=Math.pow((values[i]-avarange), 2)/(values.size()-1);
+
+		}
+
+		
+
+		return variance;
+
+		
+
+	}
+
+ static double calculeStandardDeviation(List<Double> values)
+
+	{	
+
+		return Math.sqrt(Statistical.calculeVariance(values));
+
+	}
+
+	
+
+
+    static constraints = {
+    }
+}
