d12460c5f3591a48f3efd2b212f07f7d8adf2fdb
Álex Mesquita <alex.mesquita06@hotmail.com>
2014-09-03 09:04:43 -0300

    Config database and create domain class

---
+++ grails-app/domain/com/wallet/Price.groovy
@@ -0,0 +1,13 @@
+package com.wallet
+
+class Price {
+
+	List<Date> date = new Arraylist<Date>
+	List<double> open = new Arraylist<double>
+	List<double> high = new Arraylist<double>
+	List<double> low = new Arraylist<double>
+	List<double> close = new Arraylist<double>
+	List<int> volume = new Arraylist<int>
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/com/wallet/Wallet.groovy
@@ -0,0 +1,7 @@
+package com.wallet
+
+class Wallet {
+
+    static constraints = {
+    }
+}
